153522975ea09edea6d9f448f330c3117b07f07d
krubik <krubik66@gmail.com>
2021-12-16 20:51:27 +0100

    Changes by admin

--- src/com/company/Dochod/DochodAtomowa.java
+++ src/com/company/Dochod/DochodAtomowa.java
@@ -1,9 +1,18 @@
 package com.company.Dochod;
 
 import com.company.Elektrownia;
+import com.company.ElektrowniaAtomowa;
 
 public class DochodAtomowa implements ObliczDochodElektrowni {
-    public float ObliczDochod(Elektrownia e){
-        return 0.55f * e.getMocChwilowa();
+//    public float ObliczDochod(Elektrownia e){
+//        return 0.55f * e.getMocChwilowa();
+//
+    int cenaZaWode;
+
+    @Override
+    public float ObliczDochod(Elektrownia e) {
+
+        return ((ElektrowniaAtomowa) e).getLiczbaReaktorow() * e.getMocChwilowa() * e.getDystrybutor().getCenaSkupu()
+                - ((ElektrowniaAtomowa) e).getZuzycieWody()*cenaZaWode;
     }
 }
--- src/com/company/Dochod/DochodFotowoltaiczna.java
+++ src/com/company/Dochod/DochodFotowoltaiczna.java
@@ -3,9 +3,17 @@ package com.company.Dochod;
 import com.company.Elektrownia;
 
 import com.company.Elektrownia;
+import com.company.ElektrowniaAtomowa;
+import com.company.ElektrowniaFotowoltaiczna;
 
 public class DochodFotowoltaiczna implements ObliczDochodElektrowni {
-    public float ObliczDochod(Elektrownia e){
-        return 0.5f * e.getMocChwilowa();
+//    public float ObliczDochod(Elektrownia e){
+//        return 0.5f * e.getMocChwilowa();
+//    }
+
+    @Override
+    public float ObliczDochod(Elektrownia e) {
+
+        return ((ElektrowniaFotowoltaiczna) e).getZuzyciePaneli() * e.getMocChwilowa() * e.getDystrybutor().getCenaSkupu
     }
 }
